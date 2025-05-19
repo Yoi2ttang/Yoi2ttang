84ec7d695fa3fcbb898b7672c3f2d79fb18d2db6
column-wise <cmh1199@naver.com>
2025-05-19 23:02:50 +0900

    [APP] feat: Implement android foregroundService

---
+++ app/android/app/src/main/kotlin/com/ssafy/yoittangapp/service/WearConnectionModule.kt
@@ -0,0 +1,40 @@
+package com.ssafy.yoittangapp.service
+
+import android.content.Intent
+import androidx.core.content.ContextCompat
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+import com.google.android.gms.wearable.Wearable
+
+class WearConnectionModule(reactContext: ReactApplicationContext) :
+    ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "WearConnectionModule"
+    }
+
+    @ReactMethod
+    fun getConnectedNode(promise: Promise) {
+        try {
+            val client = Wearable.getNodeClient(reactApplicationContext)
+            client.connectedNodes.addOnSuccessListener { nodes ->
+                if (nodes.isNotEmpty()) {
+
+                    val intent = Intent(reactApplicationContext, ForegroundService::class.java)
+                    ContextCompat.startForegroundService(reactApplicationContext, intent)
+                    
+                    val id = nodes.first().id
+                    promise.resolve(id)
+                } else {
+                    promise.reject("NO_NODE", "No connected nodes found.")
+                }
+            }.addOnFailureListener {
+                promise.reject("NODE_FETCH_ERROR", it.message, it)
+            }
+        } catch (e: Exception) {
+            promise.reject("EXCEPTION", e.message, e)
+        }
+    }
+}
