c1b3c9df3499555fe0759c9f100b03ffedb79a22
column-wise <cmh1199@naver.com>
2025-05-19 23:33:06 +0900

    [APP] feat: Register WearConnectionModule in MainApplication

---
+++ app/android/app/src/main/kotlin/com/ssafy/yoittangapp/WearModulePackage.kt
@@ -0,0 +1,16 @@
+package com.ssafy.yoittangapp.service
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+class WearModulePackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return listOf(WearConnectionModule(reactContext))
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList() // View 관련 모듈을 안 쓰기 때문
+    }
+}
